fe90f0a0e2439b109471219f0f9dd1600ae269e6
doulos tou Christou <jeremyhancock32@gmail.com>
2019-01-13 15:52:03 -0500

    build and troubleshooting for heroku deployment with jawsDB

---
+++ client/build/precache-manifest.c92d9fa8c927dec66e0e56749e017d58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4d28a040e4262cae9291",
+    "url": "/static/js/main.4d28a040.chunk.js"
+  },
+  {
+    "revision": "29319dd17c9f33dbb050",
+    "url": "/static/js/1.29319dd1.chunk.js"
+  },
+  {
+    "revision": "4d28a040e4262cae9291",
+    "url": "/static/css/main.022d2fe2.chunk.css"
+  },
+  {
+    "revision": "5c65696792269426040f33f2322e8969",
+    "url": "/index.html"
+  }
+];
--- scripts/seedDB.sql
+++ scripts/seedDB.sql
@@ -1,14 +1,2 @@
-CREATE DATABASE foodchain_db;
-
-USE foodchain_db;
-
-CREATE TABLE Consumer
-(
-id INT AUTO_INCREMENT UNIQUE,
-user_name varchar(45) UNIQUE,
-user_password BINARY(40),
-person_name varchar(45),
-email varchar(100) UNIQUE,
-phone_number varchar(13),
-PRIMARY KEY (id)
-)
+-- INSERT INTO products (flavor, price, quantity)
+-- VALUES ("vanilla", 2.50, 100), ("chocolate", 3.10, 120), ("strawberry", 3.25, 75);
