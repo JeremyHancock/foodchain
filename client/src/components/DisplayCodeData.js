5919e26b1b00ed72abf799797289303249c19852
doulos tou Christou <jeremyhancock32@gmail.com>
2019-01-17 14:13:16 -0500

    C:/Program Files/Git/pages/Consumer.js is getting all necessary data for displaying the foodchain from the url provided by the url scan

---
+++ client/src/components/DisplayCodeData.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function DisplayCodeData(props) {
+    return (
+        <div>
+        </div>
+    );
+}
+
+export default DisplayCodeData;
--- client/src/pages/Consumer.js
+++ client/src/pages/Consumer.js
@@ -1,12 +1,52 @@
-import React from "react";
-import ReadCode from "../components/ReadCode";
+import React, { Component } from 'react'
+import QrReader from 'react-qr-reader'
+import DisplayCodeData from '../components/DisplayCodeData'
 
-function ConsumerPage() {
-    return (
-        <div>
-            <ReadCode />
-        </div>
-    );
+const url = window.location.href;
+const urlChunks = url.split("/");
+console.log(urlChunks);
+const urlPieces = urlChunks[4].split("sirlinksalot");
+console.log(urlPieces)
+
+//urlPieces[0] === code_data
+//urlPieces[1] === product_id
+//urlPieces[2] === first link_id
+//urlPieces[3] === second link_id ...
+
+class ConsumerPage extends Component {
+    state = {
+        result: 'Please scan a product',
+        scannerOn: false,
+        code_data: urlPieces[0],
+        product_id: urlPieces[1],
+        link_id: urlPieces.slice(2)
+    }
+
+    handleScan = data => {
+        if (data) {
+            this.setState({ result: data })
+        }
+    }
+    handleError = err => { console.error(err) }
+
+    render() {
+        return (
+            <div className="scanner">
+                {this.state.scannerOn ?
+                    <QrReader
+                        delay={300}
+                        onError={this.handleError}
+                        onScan={this.handleScan}
+                        style={{ width: '100%' }}
+                    />
+                    : null
+                }
+                <p>{this.state.result}</p>
+                <DisplayCodeData
+                    {...this.state}
+                />
+            </div>
+        )
+    }
 }
-
 export default ConsumerPage;
