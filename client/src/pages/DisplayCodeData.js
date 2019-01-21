26e3228dd0f964136820e29e7ce2aa3bfa930924
doulos tou Christou <jeremyhancock32@gmail.com>
2019-01-21 14:40:21 -0500

    working, but very bloated with lots of inefficiency. It'll have to do for now

--- client/src/pages/DisplayCodeData.js
+++ client/src/pages/DisplayCodeData.js
@@ -4,14 +4,15 @@ import API from "../utils/api";
 const url = window.location.href;
 const urlChunks = url.split("/");
 const urlPieces = urlChunks[4] ? urlChunks[4].split("sirlinksalot") : [];
-// console.log(urlPieces)
-// let linkLocations = [];
-// let linkCreatedAt = [];
-// let linkNotes = [];
-// let companyNames = [];
-// let vendorLocations = [];
-// let vendorNotes = [];
-// let websites = [];
+let numberOfLinks = urlPieces.length - 2;
+console.log(numberOfLinks);
+
+let linkLocations = [];
+let linkCreatedAt = [];
+let linkNotes = [];
+let companyNames = [];
+let vendorLocations = [];
+let websites = [];
 let vendorData = [];
 let linkData = [];
 
@@ -79,11 +80,23 @@ class DisplayCodeDataPage extends Component {
             .then(res => {
                 if (res.data) {
                     productData.push(res.data);
-                    console.log("Product Data: " + JSON.stringify(productData));
+                    // console.log("Product Data: " + JSON.stringify(productData));
                     linkData.push(input.linkInfo);
-                    console.log("Link Data: " + JSON.stringify(linkData));
+                    // console.log("Link Data: " + JSON.stringify(linkData));
                     vendorData.push(input.vendorInfo);
-                    console.log("Vendor Data: " + JSON.stringify(vendorData));
+                    // console.log("Vendor Data: " + JSON.stringify(vendorData));
+                    let i;
+                    for (i = numberOfLinks - 1; i > -1; i--) {
+                        // console.log(i);
+                        linkLocations.push(linkData[0][i].location);
+                        linkCreatedAt.push(linkData[0][i].createdAt);
+                        linkNotes.push(linkData[0][i].notes);
+                        if (vendorData[i]) {
+                            companyNames.push(vendorData[i].company_name);
+                            vendorLocations.push(vendorData[i].location);
+                            websites.push(vendorData[i].website);
+                        };
+                    }
                 }
             })
             .then(() => {
@@ -94,52 +107,58 @@ class DisplayCodeDataPage extends Component {
                     certified_organic: productData[0].certified_organic,
                     vendor_notes: productData[0].vendor_notes,
                     product_createdAt: productData[0].createdAt,
-                    link_locations: linkData[0].location,
-                    link_createdAt: linkData[0].createdAt,
-                    link_notes: linkData,
-                    company_names: vendorData[0].company_name,
-                    websites: vendorData[0].website,
-                    vendor_locations: vendorData[0].location
+                    link_locations: linkLocations,
+                    link_createdAt: linkCreatedAt,
+                    // link_notes: linkNotes,
+                    company_names: companyNames,
+                    websites: websites,
+                    vendor_locations: vendorLocations
                 })
-                console.log(this.state);
+                // console.log(this.state.product_name);
+                // console.log(this.state.harvest_date);
+                // console.log(this.state.chemicals_used);
+                // console.log(this.state.certified_organic);
+                // console.log(this.state.vendor_notes);
+                // console.log(this.state.product_createdAt);
+                // console.log(this.state.link_locations);
+                // console.log(this.state.link_createdAt);
+                // console.log(this.state.link_notes);
+                // console.log(this.state.company_names);
+                // console.log(this.state.websites);
+                // console.log(this.state.vendor_locations);
             })
     }
 
     render() {
         return (
             <div>
-                {/* {this.state && this.state.linkOneVendor && */}
-                    <div>
-                        <p>{`This ${this.state.product_name} started out at ${this.state.company_names} 
-                in ${this.state.vendor_locations}. They wanted us to tell you this: "${this.state.vendor_notes}". `}
-                </p>
-                <p>{`It was harvested on ${this.state.harvest_date} 
-                and then travelled to {this.state.linkOneVendor.company_name} in {this.state.linkOneVendor.location} on {this.state.linkOneVendor.createdAt}.`}</p>
-                <p>{`From there it arrived at {this.state.linkTwoVendor.company_name} in {this.state.linkTwoVendor.location} on {this.linkThreeVendor.createdAt}.`}</p>
-                <p>{`From there it arrived at {this.state.linkThreeVendor.company_name} in {this.state.linkThreeVendor.location} on {this.state.linkThreeVendor.createdAt}.`}</p>
-                <p>{`And now, this ${this.state.product_name} is here with you! It says, "hi".`}</p>
-                        <ul>
-                            <li>{`Product Name: ${this.state.product_name}`}</li>
-                            <li>{`Harvest Date: ${this.state.harvest_date}`}</li>
-                            <li>{`Created At: ${this.state.product_createdAt}`}</li>
-                            <li>{`Grower: ${this.state.company_names}`}</li>
-                            <li>{`Organic: ${this.state.certified_organic}`}</li>
-                            <li>{`Chemicals: ${this.state.chemicals_used}`}</li>
-                            <li>{`Notes: ${this.state.vendor_notes}`}</li>
-                            {/* {this.state.link_id.map(linkItem => (
-                                <li>{`Link created at: ${linkItem.createdAt}`}</li>
-                            ))}
-                            {this.state.vendor_id.map(vendorItem => (
-                                <div>
-                                    <li>{`by vendor number: ${vendorItem.company_name}`}</li>
-                                    <li>{`vendor location: ${vendorItem.location}`}</li>
-                                    <li>{`vendor website: ${vendorItem.website}`}</li>
-                                </div>
-                            ))} */}
+                <div>
+                    <p>{`This ${this.state.product_name} started out at ${this.state.company_names[0]} 
+                in ${this.state.vendor_locations[0]}. They wanted us to tell you this: "${this.state.vendor_notes}". `}
+                    </p>
+                    <p>{`It was harvested on ${this.state.harvest_date} 
+                and then travelled to ${this.state.company_names[1]} in ${this.state.vendor_locations[1]} on ${this.state.link_createdAt[1]}.
+                `}</p>
+                    {numberOfLinks >= 2 ?
+                        <p>{`From there, it arrived at ${this.state.company_names[2]} in ${this.state.vendor_locations[2]} on ${this.state.link_createdAt[2]}.`}</p>
+                        :
+                        null}
+                    {numberOfLinks >= 3 ?
+                        <p>{`From there, it arrived at ${this.state.company_names[3]} in ${this.state.vendor_locations[3]} on ${this.state.link_createdAt[3]}.`}</p>
+                        :
+                        null}
+                    {numberOfLinks >= 4 ?
+                        <p>{`From there, it arrived at ${this.state.company_names[4]} in ${this.state.vendor_locations[4]} on ${this.state.link_createdAt[4]}.`}</p>
+                        :
+                        null}
+                    {numberOfLinks >= 5 ?
+                        <p>{`From there, it arrived at ${this.state.company_names[5]} in ${this.state.vendor_locations[5]} on ${this.state.link_createdAt[5]}.`}</p>
+                        :
+                        null}
 
-                        </ul>
-                    </div>
-                    {/* } */}
+                    <p>{`And now, this ${this.state.product_name} is here with you! It says, "hi".`}</p>
+
+                </div>
             </div >
         );
     };
