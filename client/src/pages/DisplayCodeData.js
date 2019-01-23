ea3236740e000fc5c329cba39f33c302d749863c
doulos tou Christou <jeremyhancock32@gmail.com>
2019-01-23 09:23:12 -0500

    fixed DisplayCodeData bug by chaining promises and setting state just once

--- client/src/pages/DisplayCodeData.js
+++ client/src/pages/DisplayCodeData.js
@@ -5,7 +5,7 @@ const url = window.location.href;
 const urlChunks = url.split("/");
 const urlPieces = urlChunks[4] ? urlChunks[4].split("sirlinksalot") : [];
 let numberOfLinks = urlPieces.length - 2;
-
+console.log(urlPieces);
 let linkLocations = [];
 let linkCreatedAt = [];
 let companyNames = [];
@@ -31,21 +31,41 @@ class DisplayCodeDataPage extends Component {
             code_data: urlPieces[0],
             product_id: urlPieces[1],
             link_id: urlPieces.slice(2),
-            loading: false
+            loading: true
 
         };
-        this.getProductInfo = this.getProductInfo.bind(this);
-        this.getLinkInfo = this.getLinkInfo.bind(this);
         this.getVendorInfo = this.getVendorInfo.bind(this);
     }
     componentDidMount() {
-        this.setState({ loading: true });
-        this.getLinkInfo();
         API.getProduct(this.state.product_id)
             .then(res => {
-                console.log("Product info: " + JSON.stringify(res.data));
+                // console.log("Product info: " + JSON.stringify(res.data));
                 productData.push(res.data);
-                return (res);
+            })
+            .then(() => {
+                const links = this.state.link_id;
+                links.forEach(link =>
+                    API.getLink(link)
+                        .then(res => {
+                            // console.log("Link info: " + JSON.stringify(res.data));
+                            linkCreatedAt.push(res.data.createdAt);
+                            linkLocations.push(res.data.location);
+                            this.getVendorInfo(res.data.vendor_id);
+                        })
+                )
+            })
+    };
+
+    getVendorInfo(input) {
+        const vendor = input;
+        API.getVendorById(vendor)
+            .then(res => {
+                if (res.data) {
+                    // console.log("Vendor info: " + JSON.stringify(res.data));
+                    companyNames.push(res.data.company_name);
+                    vendorLocations.push(res.data.location);
+                    websites.push(res.data.website);
+                }
             })
             .then(() => {
                 this.setState({
@@ -60,42 +80,43 @@ class DisplayCodeDataPage extends Component {
                     link_createdAt: linkCreatedAt,
                     company_names: companyNames,
                     websites: websites,
-                    vendor_locations: vendorLocations
+                    vendor_locations: vendorLocations,
                 })
             })
     };
 
-
     render() {
         return (
             <div>
-                <div>
-                    <p>{`This ${this.state.product_name} started out at ${this.state.company_names[0]} 
+                {!this.state.loading ?
+                    <div>
+                        <p>{`This ${this.state.product_name} started out at ${this.state.company_names[0]} 
                 in ${this.state.vendor_locations[0]}. They wanted us to tell you this: "${this.state.vendor_notes}". `}
-                    </p>
-                    <p>{`It was harvested on ${this.state.harvest_date} 
+                        </p>
+                        <p>{`It was harvested on ${this.state.harvest_date} 
                 and then travelled to ${this.state.company_names[1]} in ${this.state.vendor_locations[1]} on ${this.state.link_createdAt[1]}.
                 `}</p>
-                    {numberOfLinks >= 2 ?
-                        <p>{`From there, it arrived at ${this.state.company_names[2]} in ${this.state.vendor_locations[2]} on ${this.state.link_createdAt[2]}.`}</p>
-                        :
-                        null}
-                    {numberOfLinks >= 3 ?
-                        <p>{`From there, it arrived at ${this.state.company_names[3]} in ${this.state.vendor_locations[3]} on ${this.state.link_createdAt[3]}.`}</p>
-                        :
-                        null}
-                    {numberOfLinks >= 4 ?
-                        <p>{`From there, it arrived at ${this.state.company_names[4]} in ${this.state.vendor_locations[4]} on ${this.state.link_createdAt[4]}.`}</p>
-                        :
-                        null}
-                    {numberOfLinks >= 5 ?
-                        <p>{`From there, it arrived at ${this.state.company_names[5]} in ${this.state.vendor_locations[5]} on ${this.state.link_createdAt[5]}.`}</p>
-                        :
-                        null}
+                        {numberOfLinks > 2 ?
+                            <p>{`From there, it arrived at ${this.state.company_names[2]} in ${this.state.vendor_locations[2]} on ${this.state.link_createdAt[2]}.`}</p>
+                            :
+                            null}
+                        {numberOfLinks > 3 ?
+                            <p>{`From there, it arrived at ${this.state.company_names[3]} in ${this.state.vendor_locations[3]} on ${this.state.link_createdAt[3]}.`}</p>
+                            :
+                            null}
+                        {numberOfLinks > 4 ?
+                            <p>{`From there, it arrived at ${this.state.company_names[4]} in ${this.state.vendor_locations[4]} on ${this.state.link_createdAt[4]}.`}</p>
+                            :
+                            null}
+                        {numberOfLinks > 5 ?
+                            <p>{`From there, it arrived at ${this.state.company_names[5]} in ${this.state.vendor_locations[5]} on ${this.state.link_createdAt[5]}.`}</p>
+                            :
+                            null}
 
-                    <p>{`And now, this ${this.state.product_name} is here with you! It says, "hi".`}</p>
+                        <p>{`And now, this ${this.state.product_name} is here with you! It says, "hi".`}</p>
 
-                </div>
+                    </div>
+                    : <div>Loading ... </div>}
             </div >
         );
     };
