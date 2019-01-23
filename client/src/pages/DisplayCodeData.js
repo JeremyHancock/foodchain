d84ebd4c193e6e0e8c156650ed8ce93f556fdc15
doulos tou Christou <jeremyhancock32@gmail.com>
2019-01-22 21:37:12 -0500

    saving for safety

--- client/src/pages/DisplayCodeData.js
+++ client/src/pages/DisplayCodeData.js
@@ -8,13 +8,10 @@ let numberOfLinks = urlPieces.length - 2;
 
 let linkLocations = [];
 let linkCreatedAt = [];
-let linkNotes = [];
 let companyNames = [];
 let vendorLocations = [];
 let websites = [];
-let vendorData = [];
-let linkData = [];
-
+let productData = [];
 class DisplayCodeDataPage extends Component {
     constructor() {
         super();
@@ -34,6 +31,7 @@ class DisplayCodeDataPage extends Component {
             code_data: urlPieces[0],
             product_id: urlPieces[1],
             link_id: urlPieces.slice(2),
+            loading: false
 
         };
         this.getProductInfo = this.getProductInfo.bind(this);
@@ -41,65 +39,17 @@ class DisplayCodeDataPage extends Component {
         this.getVendorInfo = this.getVendorInfo.bind(this);
     }
     componentDidMount() {
+        this.setState({ loading: true });
         this.getLinkInfo();
-    };
-    getLinkInfo() {
-        let linkInfo = [];
-        const links = this.state.link_id;
-        links.forEach(link =>
-            API.getLink(link)
-                .then(res => {
-                    if (res.data) {
-                        linkInfo.push(res.data);
-                        // console.log(linkInfo);
-                        this.getVendorInfo(linkInfo);
-                    }
-                })
-        )
-    }
-    getVendorInfo(input) {
-        const vendors = input;
-        vendors.forEach(vendor =>
-            API.getVendorById(vendor.vendor_id)
-                .then(res => {
-                    if (res.data) {
-                        // console.log("This is each result (looped) for getVendorInfo: " + JSON.stringify(res.data));
-                        const getVendorResults = {
-                            linkInfo: input,
-                            vendorInfo: res.data
-                        }
-                        this.getProductInfo(getVendorResults)
-                    }
-                })
-        )
-    };
-    getProductInfo(input) {
-        const productData = [];
         API.getProduct(this.state.product_id)
             .then(res => {
-                if (res.data) {
-                    productData.push(res.data);
-                    // console.log("Product Data: " + JSON.stringify(productData));
-                    linkData.push(input.linkInfo);
-                    // console.log("Link Data: " + JSON.stringify(linkData));
-                    vendorData.push(input.vendorInfo);
-                    // console.log("Vendor Data: " + JSON.stringify(vendorData));
-                    let i;
-                    for (i = numberOfLinks - 1; i > -1; i--) {
-                        // console.log(i);
-                        linkLocations.push(linkData[0][i].location);
-                        linkCreatedAt.push(linkData[0][i].createdAt);
-                        linkNotes.push(linkData[0][i].notes);
-                        if (vendorData[i]) {
-                            companyNames.push(vendorData[i].company_name);
-                            vendorLocations.push(vendorData[i].location);
-                            websites.push(vendorData[i].website);
-                        };
-                    }
-                }
+                console.log("Product info: " + JSON.stringify(res.data));
+                productData.push(res.data);
+                return (res);
             })
             .then(() => {
                 this.setState({
+                    loading: false,
                     product_name: productData[0].product_name,
                     harvest_date: productData[0].harvest_date,
                     chemicals_used: productData[0].chemicals_used,
@@ -108,25 +58,13 @@ class DisplayCodeDataPage extends Component {
                     product_createdAt: productData[0].createdAt,
                     link_locations: linkLocations,
                     link_createdAt: linkCreatedAt,
-                    // link_notes: linkNotes,
                     company_names: companyNames,
                     websites: websites,
                     vendor_locations: vendorLocations
                 })
-                // console.log(this.state.product_name);
-                // console.log(this.state.harvest_date);
-                // console.log(this.state.chemicals_used);
-                // console.log(this.state.certified_organic);
-                // console.log(this.state.vendor_notes);
-                // console.log(this.state.product_createdAt);
-                // console.log(this.state.link_locations);
-                // console.log(this.state.link_createdAt);
-                // console.log(this.state.link_notes);
-                // console.log(this.state.company_names);
-                // console.log(this.state.websites);
-                // console.log(this.state.vendor_locations);
             })
-    }
+    };
+
 
     render() {
         return (
